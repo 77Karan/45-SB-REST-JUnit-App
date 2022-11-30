b21607642a1c697363d0d564bfdf0d04755fa4ea
karankumar <kkarankumar890@gmail.com>
2022-11-30 20:43:38 +0530

    First Commit

---
+++ src/main/java/com/ashokit/service/BookService.java
@@ -0,0 +1,16 @@
+package com.ashokit.service;
+
+import org.springframework.stereotype.Service;
+
+import com.ashokit.bindings.Book;
+
+@Service
+public class BookService
+{
+
+	public boolean saveBook(Book book) 
+	{
+		return true;
+	}
+
+}
